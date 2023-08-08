74d20b5961c9ef1eaa02ef0eb469e0454faf23dd
Micael_Henrique <133661765+Mikhailcherniev@users.noreply.github.com>
2023-08-08 10:48:08 -0300

    1

--- java.js
+++ java.js
@@ -1,18 +1,24 @@
-let button=document.getElementById("consultar")
-button.addEventListener('click', function() {
-if(hora>=6 && hora<12){
-console.log('Bom dia!')
+function verificarAcesso() {
+    var numeroCartaoInput = document.getElementById("numeroCartao");
+    var mensagemAcesso = document.getElementById("mensagemAcesso");
+    
+    var numeroCartao = parseInt(numeroCartaoInput.value);
+    
+    if (!isNaN(numeroCartao)) {
+        if (numeroCartao >= 1000 && numeroCartao <= 1999) {
+            mensagemAcesso.textContent = "Acesso total concedido a todas as áreas restritas.";
+            document.querySelector("body").style.backgroundColor  = "var(--verde)"
+        } else if (numeroCartao >= 2000 && numeroCartao <= 2999) {
+            mensagemAcesso.textContent = "Acesso limitado a algumas áreas restritas.";
+            document.querySelector("body").style.backgroundColor  = "var(--amarelo)"
+        } else if (numeroCartao > 3000) {
+            mensagemAcesso.textContent = "Acesso negado. Funcionário não tem permissão para acessar áreas restritas.";
+            document.querySelector("body").style.backgroundColor  = "var(--vermelho)"
+        } else {
+            mensagemAcesso.textContent = "Número de cartão inválido.";
+            document.querySelector("body").style.backgroundColor  = "var(--cinza)"
+        }
+    } else {
+        mensagemAcesso.textContent = "Por favor, insira um número de cartão válido.";
+    }
 }
-else if(hora>=12 && hora<18){
-console.log('Boa tarde')
-}
-else{
-console.log('Boa noite')
-}
-
-if (isNaN(resultado)){
-    resultado = "Informe um número";
- }
-
- document.querySelector('#resultado').textContent = resultado;
-})
